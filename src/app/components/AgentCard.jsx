4014dc89d317e654a86f0b30e40c9340e3f7df5a
Jonas Alberto <yonas.alluniverse@gmail.com>
2024-06-12 21:29:43 -0300

    fix: hover bug

--- src/app/components/AgentCard.jsx
+++ src/app/components/AgentCard.jsx
@@ -34,10 +34,10 @@ const AgentCard = ({ agent }) => {
           className='w-full h-full' />
       </div>
 
-      <div style={{ backgroundImage: `url(${agent.fullPortrait})` }} className={`relative bg-cover bg-center w-full h-full transition ease-out delay-[350] hover:scale-x-[-1]`} />
+      <div style={{ backgroundImage: `url(${agent.fullPortrait})` }} className={`relative bg-cover bg-center w-full h-full ${isHovered && 'transition ease-out delay-[350] scale-x-[-1]'} `} />
       <div className='flex justify-between items-center bg-[#ff4655] uppercase text-lg font-bold px-6 py-2 rounded-b'>
         <h2>{agent.displayName}</h2>
-        <Image src={`${agent.role.displayIcon}`} width={19} height={19} className='h-[19px] hidden group-hover:block' />
+        <Image src={`${agent.role.displayIcon}`} width={19} height={19} className='h-[19px] hidden group-hover:block' alt='Icone da role do agente' />
       </div>
     </div>
   )
